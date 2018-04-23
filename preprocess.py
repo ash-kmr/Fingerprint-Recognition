9db66a03c59c49210b0a755e1dfa5e458c317196
Karan <karan.shgl96@gmail.com>
2018-04-23 23:11:26 +0530

    Binary Done

--- preprocess.py
+++ preprocess.py
@@ -18,3 +18,38 @@ class Preprocess:
 		for image enhancement """
 		self.B = self.alpha + self.gamma*((self.A - self.mean)/self.stddev)
 
+	def binarization(self, q):
+		A = self.A.copy()
+		flat = A.copy().flatten()
+		hist = np.histogram(flat, bins = range(256))
+		hist_percentile = np.percentile(hist, q)
+		hist_50 = np.percentile(hist, 50)
+
+		classified = self.classified.copy()
+
+		# 1 for ridge, 2 for valley
+		ridge = (self.A <= hist_percentile)
+		valley = (self.A >= hist_50)
+		valley = valley*2
+
+		classified = ridge + valley
+
+		for i in range(A.shape[0]):
+			for j in range(A.shape[1]):
+				if classified[i,j] is 0:
+					T = classified[(i-2):(i+2), (j-2):(j+2)]
+					flat_T = T.copy().flatten()
+					hist_T = np.histogram(flat_T, bins = range(256))
+					hist_30 = np.percentile(hist_T, 30)
+
+					if A[i,j] >= hist_30:
+						# valley
+						classified[i,j] = 2
+					else:
+						classified[i,j] = 1
+
+		# 1 matlab ridge and 0 matlab valley
+		self.classified = classified == 1  
+
+
+
