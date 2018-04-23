a1864dde58ad488017fcae0439bd55674c858718
ashishkumar1550 <kumarashish1550@gmail.com>
2018-04-23 20:59:48 +0530

    fixed zhangsuen algorithm

--- zhangsuen.py
+++ zhangsuen.py
@@ -5,8 +5,8 @@ class ZhangSuen:
 		self.image = image
 		self.r, self.c = image.shape
 
-	def A(i, j):
-		canvas = self.image[(i-1)%self.r:(i+2)%self.r, (j-1)%self.c:(j+2)%self.c].copy()
+	def A(self, i, j):
+		canvas = self.image.take(range(i-1,i+2),mode='wrap', axis=0).take(range(j-1,j+2),mode='wrap',axis=1)
 		canvas = canvas.flatten().tolist()
 		canvas = canvas[1:4] + canvas[5:] + canvas[:2]
 		n_transitions = 0
@@ -19,21 +19,21 @@ class ZhangSuen:
 		cond2 = canvas[2]*canvas[4]*canvas[6]
 		cond3 = canvas[0]*canvas[2]*canvas[6]
 		cond4 = canvas[0]*canvas[4]*canvas[6]
-		return n_transitions, cond1, cond2
+		return n_transitions, cond1, cond2, cond3, cond4
 
-	def B(i, j):
+	def B(self, i, j):
 		canvas = self.image[(i-1)%self.r:(i+2)%self.r, (j-1)%self.c:(j+2)%self.c].copy()
 		return canvas.sum()
 
 	def step(self, iter):
-		mask = np.ones(self.r, self.c)
+		mask = np.ones(self.image.shape)
 		changed = 0
 		for i in range(self.r):
 			for j in range(self.c):
-				n_transitions, cond1, cond2, cond3, cond4 = A(i, j)
-				if self.image[i, j] != 1 and B(i, j) > 1:
+				n_transitions, cond1, cond2, cond3, cond4 = self.A(i, j)
+				if self.image[i, j] != 1 and self.B(i, j) > 1:
 					continue
-				if B(i, j) < 2 or B(i, j) > 6:
+				if self.B(i, j) < 2 or self.B(i, j) > 6:
 					continue
 				if n_transitions != 1:
 					continue
@@ -46,14 +46,15 @@ class ZhangSuen:
 				if cond4 != 0 and iter == 2:
 					continue
 				mask[i, j] = 0
-				changed = 1
+				changed += 1
 		self.image = self.image*mask
 		return changed
 
 	def performThinning(self):
 		while True:
-			step1 = step(1)
-			step2 = step(2)
+			step1 = self.step(1)
+			step2 = self.step(2)
+			print(step1+step2)
 			if step1 == 0 and step2 == 0:
 				break
 
